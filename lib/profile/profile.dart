40adb86b02d23847adf0a17ed3c3f8934e18beed
Sergey Salnikov <ncr98@yandex.ru>
2021-05-02 01:35:43 +0300

    added profile feature

---
+++ lib/profile/profile.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'src/content.dart';
+
+class ProfilePage extends MaterialPage {
+  ProfilePage() : super(child: ProfileContent());
+}
---
+++ lib/profile/src/content.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// ignore: use_key_in_widget_constructors
+class ProfileContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('$runtimeType'),
+      ),
+    );
+  }
+}
